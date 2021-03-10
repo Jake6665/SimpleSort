b8c7ba794c377eabc57f725633e16905afb132c3
Jake6665 <61512633+Jake6665@users.noreply.github.com>
2021-03-09 22:30:03 -0500

    SortMe Program Done

---
+++ sort_list.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace JakobThrasher_Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                string[] lines = File.ReadAllLines("Sort Me.txt");
+
+                Array.Sort(lines);
+
+                string[] newLine = new string[lines.Length];
+
+                int pos = 0;
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    for (int j = 0; j < lines.Length; j++)
+                    {
+                        if (lines[j].Length == i)
+                        {
+                            newLine[pos] = lines[j];
+                            pos++;
+                        }
+
+                        if (pos >= newLine.Length)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                foreach (string line in newLine)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
