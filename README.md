db1ccf319141a1faff4be622c1c49ea5e7999045
Jake6665 <61512633+Jake6665@users.noreply.github.com>
2021-03-17 14:17:24 -0400

    3/17/21 Update 1
    Reverse Sort application added and instructions updated.

--- README.md
+++ README.md
@@ -4,9 +4,17 @@
 1: Ability to run C# programs from command prompt.
 2: Latest version of Visual Studio.
 
-## Instructions to run Simple Sort through Command Prompt with C#:
+## Initial Instructions for Setup
 1: Copy and extract the "SimpleSort" file from either email or GitHub.
 2: Navigate to "SimpleSort" folder in the command prompt.
-3: If needed, set up application by typing "csc sort_list.cs" and pressing enter
-4: Run application by typing "sort_list" and pressing enter
-5: To compare sorted list to unsorted list type "type "Sort Me".txt".
+
+## Instructions to run Simple Sort through Command Prompt with C#:
+1: If needed, set up application by typing "csc sort_list.cs" and pressing enter
+2: Run application by typing "sort_list" and pressing enter
+3: To compare sorted list to unsorted list type "type "Sort Me".txt".
+
+## Instructions to run Reverse Simple Sort through Command Prompt with C#:
+1: If needed, set up application by typing "csc reverse_sort_list.cs" and pressing enter
+2: Run application by typing "reverse_sort_list" and pressing enter
+3: To compare sorted list to unsorted list type "type "Sort Me".txt".
+4: To compare sorted list to regular sorted list type "sort_list.
---
+++ reverse_sort_list.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace JakobThrasher_Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                string[] lines = File.ReadAllLines("Sort Me.txt");
+
+                Array.Sort(lines);
+
+                string[] newLine = new string[lines.Length];
+
+                int pos = 0;
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    for (int j = 0; j < lines.Length; j++)
+                    {
+                        if (lines[j].Length == i)
+                        {
+                            newLine[pos] = lines[j];
+                            pos++;
+                        }
+
+                        if (pos >= newLine.Length)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                string[] reverseLine = new string[lines.Length];
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    reverseLine[reverseLine.Length-1-i] = newLine[i];
+                }
+
+                foreach (string line in reverseLine)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
