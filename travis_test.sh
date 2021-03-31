335ce7c517cee0afbde8086fc0a9a92e43b04ea1
Jake6665 <61512633+Jake6665@users.noreply.github.com>
2021-03-31 17:42:32 -0400

    Update travis_test.sh

--- travis_test.sh
+++ travis_test.sh
@@ -1,8 +1,8 @@
 
-mono sort_list < "Sort Me".txt > "Output_1.txt"
-mono reverse_sort_list < "Sort Me".txt > "Output_2".txt
+mono sort_list.exe < "Sort Me".txt > "Output_1.txt"
+mono reverse_sort_list.exe < "Sort Me".txt > "Output_2".txt
 
-mono fc "Output_1".txt "Sorted Text".txt
+mono diff "Output_1".txt "Sorted Text".txt
 mono IF %ERRORLEVEL% EQU 0 (Echo Sort Pass) ELSE (Echo Sort Failed)
-mono fc "Output_2".txt "Reversed Sorted Text".txt
+mono diff "Output_2".txt "Reversed Sorted Text".txt
 mono IF %ERRORLEVEL% EQU 0 (Echo Sort Pass) ELSE (Echo Sort Failed)
