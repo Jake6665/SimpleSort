a1e2db899fce5eab1749b2f4e248175db159120c
Jake6665 <61512633+Jake6665@users.noreply.github.com>
2021-03-31 17:51:24 -0400

    Update travis_test.sh

--- travis_test.sh
+++ travis_test.sh
@@ -2,5 +2,5 @@
 mono sort_list.exe < "Sort Me".txt > "Output_1.txt"
 mono reverse_sort_list.exe < "Sort Me".txt > "Output_2".txt
 
-mono diff "Output_1".txt "Sorted Text".txt
-mono diff "Output_2".txt "Reversed Sorted Text".txt
+diff "Output_1".txt "Sorted Text".txt
+diff "Output_2".txt "Reversed Sorted Text".txt
