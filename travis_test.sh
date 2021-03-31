938d1141cd3f126e21ce809db5d1242b098de12c
Jake6665 <61512633+Jake6665@users.noreply.github.com>
2021-03-31 18:10:30 -0400

    Update travis_test.sh

--- travis_test.sh
+++ travis_test.sh
@@ -1,3 +1,7 @@
+sudo apt update
+sudo apt install mono-complete
+mcs -out:sort_list.exe sort_list.cs
+mcs -out:reverse_sort_list.exe reverse_sort_list.cs
 
 mono sort_list.exe < "Sort Me".txt > "Output_1.txt"
 mono reverse_sort_list.exe < "Sort Me".txt > "Output_2".txt
