d13aeb7f422a8aab043c1583620b52fc607991fe
Jake6665 <61512633+Jake6665@users.noreply.github.com>
2021-03-31 16:49:04 -0400

    Delete command.sh

--- command.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update
-sudo apt install mono-complete
-mcs -out:sort_list.exe sort_list.cs
-mono sort_list.exe
-mcs -out:reverse_sort_list.exe reverse_sort_list.cs
-mono reverse_sort_list.exe
-mono test
